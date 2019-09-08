bb38da456a56ea2635ee6cb3d16288ae5f1d3f4f
dominik-wbz <dominiknguyen@icloud.com>
2019-09-08 17:24:04 +0200

    fix

--- src/LaravelFakturoid.php
+++ src/LaravelFakturoid.php
@@ -8,16 +8,16 @@ class LaravelFakturoid {
 
 	public function __construct() {
 		$this->config = [
-			'account_name' => config('fakturoid.app_name'),
-			'account_email' => config('fakturoid.app_email'),
-			'account_api_key' => config('fakturoid.app_api_key'),
+			'account_name' => config('fakturoid.account_name'),
+			'account_email' => config('fakturoid.account_email'),
+			'account_api_key' => config('fakturoid.account_api_key'),
 			'app_contact' => config('fakturoid.app_contact'),
 		];
 		$this->initFakturoid();
 	}
 
 	protected function initFakturoid() {
-		$this->fakturoid = new Fakturoid\Client($this->config['app_name'], $this->config['app_email'], $this->config['app_api_key'], $this->config['app_contact']);
+		$this->fakturoid = new Fakturoid\Client($this->config['account_name'], $this->config['account_email'], $this->config['account_api_key'], $this->config['app_contact']);
 
 		return $this->fakturoid;
 	}
