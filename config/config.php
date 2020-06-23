7eaf68c905e1c15355d65849f0081fd7ed8c4672
Jakub Theimer <theimer@madne.st>
2020-06-23 20:03:35 +0200

    move config and add env variables

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'account_name' => env('FAKTUROID_NAME', 'XXX'),
+    'account_email' => env('FAKTUROID_EMAIL', 'XXX'),
+    'account_api_key' => env('FAKTUROID_API_KEY', 'XXX'),
+    'app_contact' => env('FAKTUROID_APP_CONTACT', 'Application <your@email.cz>'),
+];
--- src/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-	'account_name' => 'XXX',
-	'account_email' => 'XXX',
-	'account_api_key' => 'XXX',
-	'app_contact' => 'PHPlib <your@email.cz>',
-];
