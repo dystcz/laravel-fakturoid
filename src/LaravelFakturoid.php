6c57e8f8a6100201e3110a0243cb6dca895b44e1
Jakub Theimer <theimer@madne.st>
2020-06-23 20:04:14 +0200

    indent with 4 spaces

--- src/LaravelFakturoid.php
+++ src/LaravelFakturoid.php
@@ -4,33 +4,35 @@ namespace WEBIZ\LaravelFakturoid;
 
 use Fakturoid;
 
-class LaravelFakturoid {
+class LaravelFakturoid
+{
+    public function __construct()
+    {
+        $this->config = [
+            'account_name' => config('fakturoid.account_name'),
+            'account_email' => config('fakturoid.account_email'),
+            'account_api_key' => config('fakturoid.account_api_key'),
+            'app_contact' => config('fakturoid.app_contact'),
+        ];
+        $this->initFakturoid();
+    }
 
-	public function __construct() {
-		$this->config = [
-			'account_name' => config('fakturoid.account_name'),
-			'account_email' => config('fakturoid.account_email'),
-			'account_api_key' => config('fakturoid.account_api_key'),
-			'app_contact' => config('fakturoid.app_contact'),
-		];
-		$this->initFakturoid();
-	}
+    protected function initFakturoid()
+    {
+        $this->fakturoid = new Fakturoid\Client($this->config['account_name'], $this->config['account_email'], $this->config['account_api_key'], $this->config['app_contact']);
 
-	protected function initFakturoid() {
-		$this->fakturoid = new Fakturoid\Client($this->config['account_name'], $this->config['account_email'], $this->config['account_api_key'], $this->config['app_contact']);
+        return $this->fakturoid;
+    }
 
-		return $this->fakturoid;
-	}
-
-	public function __call($name, $arguments) {
-		if (method_exists($this, $name)) {
-			return $this->{$name}(...$arguments);
-		} else if (method_exists($this->fakturoid, $name)) {
-			$fakturoid = $this->fakturoid;
-			$methodResult = $fakturoid->{$name}(...$arguments);
-			return $methodResult;
-		}
-		return null;
-	}
-
+    public function __call($name, $arguments)
+    {
+        if (method_exists($this, $name)) {
+            return $this->{$name}(...$arguments);
+        } else if (method_exists($this->fakturoid, $name)) {
+            $fakturoid = $this->fakturoid;
+            $methodResult = $fakturoid->{$name}(...$arguments);
+            return $methodResult;
+        }
+        return null;
+    }
 }
